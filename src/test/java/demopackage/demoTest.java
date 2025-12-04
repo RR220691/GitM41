4cf9e86c2f391a48b04cb79d4efe8ae397465495
raksh <raksh@Rakshi>
2025-12-04 11:20:43 +0530

    commited

---
+++ src/test/java/demopackage/demoTest.java
@@ -0,0 +1,12 @@
+package demopackage;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+	@Test(groups="smoke")
+	public void dhoni()
+	{
+		System.out.println("dhoni");
+	}
+
+}
---
+++ src/test/java/demopackage/sampleTest.java
@@ -0,0 +1,13 @@
+package demopackage;
+
+import org.testng.annotations.Test;
+
+public class sampleTest {
+	
+	@Test(groups="Regression")
+	public void virat()
+	{
+		System.out.println("virat");
+	}
+
+}
